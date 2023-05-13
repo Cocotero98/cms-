1b623260072a13ae0f171aa0364559bf39529339
Cocotero98 <aguilarag98@gmail.com>
2023-05-12 23:59:04 -0300

    Documents components implemented

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    {id: 1, name:'OOP', description: 'this is de description of OOP', url: 'docs/oop.pdf', children:[{test:'test'}]},
+    {id: 1, name:'test', description: 'this is a test', url: 'test/url', children:[{test:'test'}]},
+    {id: 1, name:'WSD', description: 'this is a test decription for WSD', url: 'docs/wsd.pdf', children:[{test:'test'}]},
+    {id: 1, name:'EP', description: 'I am testing this description', url: 'docs/EP.pdf', children:[{test:'test'}]}
+  ] 
 
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
+  }
 }
