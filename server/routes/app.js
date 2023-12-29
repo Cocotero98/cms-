2e07b8ec021e81a3e155dcbc92405b8bf1d67afa
Cocotero98 <aguilarag98@gmail.com>
2023-12-29 15:57:00 -0300

    .

--- server/routes/app.js
+++ server/routes/app.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.resolve(__dirname, 'dist/cms/index.html'));
-});
+// router.get('/', function(req, res, next) {
+//   // res.sendFile(path.resolve(__dirname, 'dist/cms/index.html'));
+// });
 
 module.exports = router;
